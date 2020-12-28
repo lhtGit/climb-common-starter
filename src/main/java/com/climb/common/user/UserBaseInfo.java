7df9159a41d8f9b2206eb2c3a8f49dd6018cf821
linghaitian <Xhmh123456>
2020-12-28 11:26:09 +0800

    gateway完成，实现登录+权限认证功能

--- src/main/java/com/climb/common/user/UserBaseInfo.java
+++ src/main/java/com/climb/common/user/UserBaseInfo.java
@@ -1,20 +1,35 @@
 package com.climb.common.user;
 
 
+import lombok.Data;
+
+import java.io.Serializable;
+
 /**
  * 用户基础信息
  * @author lht
  * @since 2020/9/28 10:39
  */
-public interface UserBaseInfo {
+@Data
+public class UserBaseInfo implements Serializable{
 
-    String getId();
+    private static final long serialVersionUID = -8717357144429994025L;
+    /**
+     * 用户标识
+     * @author lht
+     * @since  2020/11/26 11:48
+     */
+    private String id;
+    /**
+     * 用户昵称
+     * @author lht
+     * @since  2020/11/26 11:49
+     */
+    private String name;
 
-    String getName();
+    private String username;
 
-    String getPhone();
+    private String phone;
 
-    String getEmail();
-
-    String getUsername();
+    private String email;
 }
--- src/main/java/com/climb/common/user/util/UserUtils.java
+++ src/main/java/com/climb/common/user/util/UserUtils.java
@@ -3,7 +3,6 @@ package com.climb.common.user.util;
 import com.alibaba.fastjson.JSON;
 import com.climb.common.constant.CommonConstant;
 import com.climb.common.user.UserBaseInfo;
-import com.climb.common.user.bean.DefaultUserBaseInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.StringUtils;
 
@@ -29,18 +28,18 @@ public class UserUtils {
      * @since  2020/11/26 14:44
      */
     public static UserBaseInfo getUserDetails(HttpServletRequest request){
-        DefaultUserBaseInfo userInfo = null;
+        UserBaseInfo userInfo = null;
         String userStr = request.getHeader(CommonConstant.USER_INFO);
         if(!StringUtils.isEmpty(userStr)){
             try{
-                userInfo = JSON.parseObject(URLDecoder.decode(userStr,CommonConstant.UTF8), DefaultUserBaseInfo.class);
+                userInfo = JSON.parseObject(URLDecoder.decode(userStr,CommonConstant.UTF8), UserBaseInfo.class);
             }catch (Exception e){
                 log.error("获取用户信息失败",e);
             }
         }
         //设置未登录用户信息
         if(userInfo==null){
-            userInfo = new DefaultUserBaseInfo();
+            userInfo = new UserBaseInfo();
             userInfo.setId(NONE_USER_ID);
             userInfo.setName("未登录");
         }
