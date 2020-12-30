9459a820faf0646eeda77a08b42aecfc899799a0
linghaitian <Xhmh123456>
2020-12-30 17:02:04 +0800

    gateway增加扩展登录方式机制 redis、gateway、swagger增加spring-configuration-metadata.json实现配置提示

--- src/main/java/com/climb/common/user/auth/UserLoginType.java
+++ src/main/java/com/climb/common/user/auth/UserLoginType.java
@@ -7,8 +7,6 @@ package com.climb.common.user.auth;
 public enum UserLoginType {
     /**
      * 用户密码类型登录
-     * @author lht
-     * @since  2020/11/26 16:28
      */
     USERNAME_PASSOWRD,
     ;
