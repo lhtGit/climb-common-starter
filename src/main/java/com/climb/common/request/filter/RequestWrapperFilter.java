119d37918e80e858770c9d7b7b0573ecd320a052
linghaitian <Xhmh123456>
2021-03-05 14:44:01 +0800

    解决参数转化

--- src/main/java/com/climb/common/request/filter/RequestWrapperFilter.java
+++ src/main/java/com/climb/common/request/filter/RequestWrapperFilter.java
@@ -10,11 +10,17 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 /**
+ * 包装request请求
+ * 在包装请求时需要注意不能再HiddenHttpMethodFilter之前包装，应该HiddenHttpMethodFilter过滤器是转化参数请求的，避免因为包装请求后不能正确转化参数
+ *
+ * HiddenHttpMethodFilter：
+ *  浏览器form表单只支持GET与POST请求，而DELETE、PUT等method并不支持，spring3.0添加了一个过滤器，
+ *  可以将这些请求转换为标准的http方法，使得支持GET、POST、PUT与DELETE请求，该过滤器为HiddenHttpMethodFilter
  * @author lht
  * @since 2021/3/1 14:16
  */
 @WebFilter
-@Order(Integer.MIN_VALUE)
+@Order
 public class RequestWrapperFilter implements Filter {
 
     @Override
